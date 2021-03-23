ec8b69fa226f35aa9c6c84ea4d4783b2d95a2712
benchan777 <benchan777@gmail.com>
2021-03-23 04:13:39 -0700

    add comments so i can read my spaghetti code later

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,77 @@
-from bot.models import BreadStock
+from bot.models import Base, CountryLoaf
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import discord
 from discord.ext import commands
 import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
 
 bot = commands.Bot(command_prefix = '$', intents = discord.Intents.all())
 engine = create_engine('sqlite:///database.db')
-Base.metadata.creat_all(engine)
+Base.metadata.create_all(engine)
 Session = sessionmaker(engine)
 Session.configure(bind = engine)
 db = Session()
+from bot.functions import store_info_embed
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+
+@bot.command(pass_context = True)
+async def test(ctx, *args):
+    database_test = CountryLoaf(
+        availability = ' '.join(args),
+        time = '8:00pm'
+    )
+    db.add(database_test)
+    db.commit()
+
+    await ctx.send(db.query(CountryLoaf.availability).filter_by(time = '8:00pm').first()[0])
+
+@bot.command()
+async def selenium_test(ctx):
+    driver = webdriver.Chrome(os.getenv('webdriver_path'))
+    driver.get("https://guerrero.tartine.menu/pickup/")
+
+    items = driver.find_elements_by_class_name('menu-item-heading') #Retrieves item name
+    descriptions = driver.find_elements_by_class_name('menu-item-description') #Retrieves item description
+    prices = driver.find_elements_by_class_name('pricecolor') #Retrieves item price
+
+    #Jank way to find if each item is in stock. Check if 'Not Available' appears anywhere within the clickable box. If not, assume item is available
+    stock = driver.find_elements_by_class_name('mb12m')
+
+    #Keep track of which item we're on in list of items
+    index = 0
+
+    for item in items:
+        try:
+            description = descriptions[index].text
+        except:
+            description = 'N/A'
+
+        try:
+            price = prices[index].text
+        except:
+            price = 'N/A'
+
+        availability = 'Not Available' if 'Not Available' in stock[index].text else 'Available'
+
+        embed = store_info_embed(
+            item.text, 
+            description, 
+            price, 
+            availability,
+            0x00ff00 if availability == 'Available' else 0xff0000
+            )
+
+        # await ctx.send(f"**Name: {item.text}**\nDescription: {description}\nPrice: {price}\nAvailability: {availability}")
+        await ctx.send(embed = embed)
+
+        index += 1
+
+    driver.close()
