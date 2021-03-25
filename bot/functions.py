b006d4217c5c42a149ea45b7e05ed6e104ef28f3
benchan777 <benchan777@gmail.com>
2021-03-25 03:24:06 -0700

    add thumbnail to embed

--- bot/functions.py
+++ bot/functions.py
@@ -4,7 +4,7 @@ import datetime, discord, pytz
 from discord.ext import commands
 
 #Function that helps to create discord embeds for item info
-def store_info_embed(name, description, price, availability, color):
+def store_info_embed(name, description, thumbnail, price, availability, color):
     embed_1 = 'Error' if len(str(name)) == 0 else name
     embed_2 = 'Error' if len(str(description)) == 0 else description
     embed_3 = 0xffff00 if len(str(color)) == 0 else color
@@ -12,7 +12,7 @@ def store_info_embed(name, description, price, availability, color):
     embed_5 = 'Error' if len(str(availability)) == 0 else availability
 
     embed = discord.Embed(title = embed_1, description = embed_2, color = embed_3)
-    # embed.set_thumbnail(url = thumbnail)
+    embed.set_thumbnail(url = thumbnail)
 
     embed.add_field(name = 'Price', value = price, inline = True)
     embed.add_field(name = 'Availability', value = availability, inline = True)
